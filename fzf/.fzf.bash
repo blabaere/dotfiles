fd34dbb1ddeaaf64cf719367f59ed8ace6981a34
Benoît Labaere <benoit.labaere+github@lspdtc.fr>
2018-11-03 21:49:50 +0100

    Adds fzf bash

---
+++ fzf/.fzf.bash
@@ -0,0 +1,15 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/blabaere/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/blabaere/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/blabaere/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/blabaere/.fzf/shell/key-bindings.bash"
+
+export FZF_DEFAULT_COMMAND='rg --files --hidden'
