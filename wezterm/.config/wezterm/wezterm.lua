108c701c6e2cb4e9ab91ce127ad082ec75e699b9
blabaere <benoit.labaere@lspdtc.fr>
2021-12-03 14:59:24 +0100

    Updates wezterm keyboard shortcuts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,12 +2,23 @@ local wezterm = require 'wezterm';
 
 return {
 	font = wezterm.font("Fira Code"),
-	font_size = 11,
+	font_size = 12,
 	--color_scheme = "Tomorrow Night",
 	color_scheme = "Solarized Dark Higher Contrast",
 	debug_key_events = false,
 	keys = {
+		-- Ctrl + Shift + 't' : create tab
+
+		-- Ctrl + Shift + 'ù' : create split to the right
 		{key="raw:48", mods="CTRL|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-		{key="raw:12", mods="CTRL|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+
+		-- Ctrl + Shift + '*' : create split below
+		{key="raw:51", mods="CTRL|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+
+		-- Ctrl + Shift + ')' : move to left tab
+		{key="raw:20", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
+
+		-- Ctrl + Shift + '=' : move to right tab
+		{key="raw:21", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1}},
 	},
 }
